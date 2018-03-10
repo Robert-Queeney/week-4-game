6ca8b89c3c92933b1cf12241201aaca85265956b
scoslo5512 <robert.queeney@gmail.com>
2018-03-10 10:28:00 -0800

    Added js to calculate wins losses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -35,38 +35,55 @@ $(`#crystal1`).click(function() {
     totalScore = totalScore + crystalRandom1 
     $(`#ts`).text(totalScore); 
     console.log("total score", totalScore); 
+    conclusion(); 
+    
 })
 
 $(`#crystal2`).click(function() {
     totalScore = totalScore + crystalRandom2 
     $(`#ts`).text(totalScore); 
     console.log("total score", totalScore); 
+    conclusion(); 
+    
 })
 
 $(`#crystal3`).click(function() {
     totalScore = totalScore + crystalRandom3
     $(`#ts`).text(totalScore); 
     console.log("total score", totalScore); 
+    conclusion(); 
+    
 })
 
 $(`#crystal4`).click(function() {
     totalScore = totalScore + crystalRandom4
     $(`#ts`).text(totalScore); 
     console.log("total score", totalScore); 
+    conclusion(); 
+     
 })
 
 // wins losses 
 function conclusion() {
     if (totalScore > randomNumber){
          losses ++ ; 
-         console.log(losses); 
-        $(`#losses`).text(wins); 
+         console.log(losses, wins); 
+        $(`#losses`).text(losses); 
+        location.reload(); 
     } else if (totalScore === randomNumber){
          wins ++ ; 
-        $(`#wins`).text(losses); 
+        $(`#wins`).text(wins); 
+        location.reload(); 
     }
 }
-conclusion(); 
+
+// trying to get the page to reload after a win or loss
+// function restart(){
+//     if (wins ++ || losses ++){
+//         location.reload(); 
+//     }
+// }
+
 
 console.log("random number", randomNumber); 
 
